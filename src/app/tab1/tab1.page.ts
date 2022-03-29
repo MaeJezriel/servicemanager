7d6c05b7b1f1b9ac43d8c836e54f0b0f27cdca80
MaeJezriel <201810487@gordoncollege.edu.ph>
2022-03-29 09:49:57 +0800

    Initial commit

---
+++ src/app/tab1/tab1.page.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { Tab2Page } from '../tab2/tab2.page';
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss']
+})
+export class Tab1Page {
+
+  constructor(private modalCtrl: ModalController, private modalController: ModalController) {}
+
+  ngOnInit() { }
+
+
+  // async view() {
+  //   const modal = await this.modalCtrl.create({
+  //     component: JobSummaryPage,
+  //     cssClass: 'my-custom-class'
+  //   });
+  //   return await modal.present();
+  // }
+
+  public async openJobSummary() {
+    const modal = await this.modalController.create({
+      component: Tab2Page,
+      cssClass: 'my-custom-class'
+    });
+    return await modal.present();
+  }
+
+}
+
+
+
+
+
+
+
---
+++ src/app/tab2/tab2.page.ts
@@ -0,0 +1,86 @@
+import { Component, ViewChild } from '@angular/core';
+import { ActionSheetController, IonSlides, ModalController } from '@ionic/angular';
+import { UserPhoto, PhotoService } from '../services/photo.service';
+
+@Component({
+  selector: 'app-tab2',
+  templateUrl: 'tab2.page.html',
+  styleUrls: ['tab2.page.scss']
+})
+export class Tab2Page {
+
+  @ViewChild("SwipedSliderContainer", { static: true })
+  public SwipedSliderContainer: IonSlides;
+
+  public tabs: any = [];
+  public tabIndex: number;
+
+  constructor(private modalCtrl: ModalController,
+    public photoService: PhotoService, public actionSheetController: ActionSheetController) {}
+
+  public segmentChange(event) {
+    const newtab = event._value;
+    // slider.slideTo(newtab);
+  }
+
+  //dismiss
+  public dismiss(): void {
+    this.modalCtrl.dismiss();
+ }
+ segmentModel = "summary";
+
+  segmentChanged(event){
+    console.log(this.segmentModel);
+    
+    console.log(event);
+  }
+
+
+  // //modal
+  // public async cleaning() {
+  //   const modal = await this.modalCtrl.create({
+  //     component: CleaningPage,
+  //     cssClass: 'my-custom-class'
+  //   });
+  //   return await modal.present();
+  // }
+
+  //  //modal
+  //  public async calibration() {
+  //   const modal = await this.modalCtrl.create({
+  //     component: CalibrationPage,
+  //     cssClass: 'my-custom-class'
+  //   });
+  //   return await modal.present();
+  // }
+
+  //image
+  async ngOnInit() {
+    await this.photoService.loadSaved();
+  }
+
+  public async showActionSheet(photo: UserPhoto, position: number) {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Photos',
+      buttons: [{
+        text: 'Delete',
+        role: 'destructive',
+        icon: 'trash',
+        handler: () => {
+          this.photoService.deletePicture(photo, position);
+        }
+      }, {
+        text: 'Cancel',
+        icon: 'close',
+        role: 'cancel',
+        handler: () => {
+          // Nothing to do, action sheet is automatically closed
+         }
+      }]
+    });
+    await actionSheet.present();
+  }
+}
+
+
+
