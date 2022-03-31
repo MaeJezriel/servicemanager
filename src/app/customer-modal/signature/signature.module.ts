5212792107e58fd5bb4789f3c38a79874a8de266
MaeJezriel <201810487@gordoncollege.edu.ph>
2022-03-31 08:25:48 +0800

    working signature pad

---
+++ src/app/customer-modal/signature/signature.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { IonicModule } from '@ionic/angular';
+
+import { SignaturePageRoutingModule } from './signature-routing.module';
+
+import { SignaturePadModule } from 'angular2-signaturepad';
+import { SignaturePage } from './signature.page';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    SignaturePadModule,
+  ],
+  declarations: [SignaturePage]
+})
+export class SignaturePageModule {}
---
+++ src/app/customer-modal/signature/signature.page.ts
@@ -0,0 +1,54 @@
+import { AfterViewInit, Component, ElementRef, ViewChild, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { SignaturePad } from 'angular2-signaturepad';
+
+@Component({
+  selector: 'app-signature',
+  templateUrl: './signature.page.html',
+  styleUrls: ['./signature.page.scss'],
+})
+export class SignaturePage implements OnInit,  AfterViewInit {
+
+  @ViewChild(SignaturePad) signaturePad: SignaturePad;
+  signatureImg : string;
+  signaturePadOptions: Object = { 
+    'minWidth': 5,
+    'canvasWidth': 500,
+    'canvasHeight': 300
+  };
+  constructor(private modalCtrl: ModalController) { }
+
+  ngOnInit() {
+  }
+
+
+//dismiss
+public dismiss(): void {
+  this.modalCtrl.dismiss();
+}
+
+
+ngAfterViewInit() {
+  // this.signaturePad is now available
+  this.signaturePad.set('minWidth', 5); // set szimek/signature_pad options at runtime
+  this.signaturePad.clear(); // invoke functions from szimek/signature_pad API
+}
+
+drawComplete() {
+  // will be notified of szimek/signature_pad's onEnd event
+  console.log(this.signaturePad.toDataURL());
+}
+
+drawStart() {
+  // will be notified of szimek/signature_pad's onBegin event
+  console.log('begin drawing');
+}
+
+clearPad() {
+  this.signaturePad.clear();
+}
+
+savePad() {
+  this.signatureImg = this.signaturePad.toDataURL();
+}
+}
