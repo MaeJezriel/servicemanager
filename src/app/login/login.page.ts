fc67dca0a70bd99e862041ca8b5b01d281783c7d
MaeJezriel <201810487@gordoncollege.edu.ph>
2022-04-07 09:31:11 +0800

    static login for now

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { AuthService } from '../services/auth.service';
+// import { AuthService } from '../services/auth.service';
 import { AlertController, LoadingController, ToastController  } from '@ionic/angular';
 import { Router } from '@angular/router';
  
@@ -15,7 +15,7 @@ export class LoginPage implements OnInit {
   };
  
   constructor(
-    private auth: AuthService,
+    //private auth: AuthService,
     private router: Router,
     private alertCtrl: AlertController,
     public toastController: ToastController,
@@ -24,32 +24,32 @@ export class LoginPage implements OnInit {
  
   ngOnInit() {}
  
-  login() {
-    this.auth.login(this.credentials).subscribe(async res => {
-      if (res) {
-        this.router.navigateByUrl('/members');
-      } else {
-        const alert = await this.alertCtrl.create({
-          header: 'Login Failed',
-          message: 'Please provide all the required values!',
-          buttons: ['OK']
-        });
-        await alert.present();
-      }
-    });
-  }
+  // login() {
+  //   this.auth.login(this.credentials).subscribe(async res => {
+  //     if (res) {
+  //       this.router.navigateByUrl('/members');
+  //     } else {
+  //       const alert = await this.alertCtrl.create({
+  //         header: 'Login Failed',
+  //         message: 'Please provide all the required values!',
+  //         buttons: ['OK']
+  //       });
+  //       await alert.present();
+  //     }
+  //   });
+  // }
 
-  async successLoading() {
-    const loading = await this.loadingController.create({
-      cssClass: 'my-custom-class',
-      message: 'Please wait...',
-      duration: 2000
-    });
-    await loading.present();
+  // async successLoading() {
+  //   const loading = await this.loadingController.create({
+  //     cssClass: 'my-custom-class',
+  //     message: 'Please wait...',
+  //     duration: 2000
+  //   });
+  //   await loading.present();
 
-    await loading.onDidDismiss().then(() => {
-      this.router.navigateByUrl('/members');
-    });
-  }
+  //   await loading.onDidDismiss().then(() => {
+  //     this.router.navigateByUrl('/members');
+  //   });
+  // }
  
 }
